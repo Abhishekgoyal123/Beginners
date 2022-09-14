633186727db9a98c65aca9e277ee2bd28c14322e
Abhishek Goyal <abhishek.goyal@coditas.com>
2022-09-14 17:42:57 +0530

    14/09

--- Date_Time/Program.cs
+++ Date_Time/Program.cs
@@ -5,5 +5,26 @@ DateTime dt = new DateTime();
 
 var now = DateTime.Now;
 
-Console.WriteLine($"current time = {now.Hour});
+Console.WriteLine($"current time = {now.Hour}");
 
+Console.WriteLine(now.ToLongDateString());
+
+Console.WriteLine(now.ToShortDateString());
+
+Console.WriteLine(now.ToShortTimeString());
+
+Console.WriteLine(now.ToLongTimeString());
+
+Console.WriteLine( now.ToString("yyyy:MM:dd") );
+
+TimeSpan ts = new TimeSpan(1,2,3);
+
+Console.WriteLine(ts);
+
+var ts1 = TimeSpan.FromHours(1);
+
+Console.WriteLine(ts1);
+
+Console.WriteLine($"minutes = {ts.Minutes}");
+
+Console.WriteLine($"add ={ts.Add(TimeSpan.FromMinutes(1))}");
