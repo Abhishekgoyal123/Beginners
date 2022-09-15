428dfaf25a098340df924f60be733f57f218445c
Abhishek Goyal <abhishek.goyal@coditas.com>
2022-09-15 16:23:16 +0530

    15/09

---
+++ Exercise_6_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] arr = new int[5];
+
+Console.WriteLine("enter numbers");
+int i = 0;
+
+for(i = 0; i < arr.Length; i++)
+{
+    arr[0] = Convert.ToInt32(arr[i]);   
+
+}
+
